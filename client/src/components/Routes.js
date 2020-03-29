78c75b28e3a825031f8780f273e1a74e0b85ff94
PunitLodha <punitlodha@pm.me>
2020-03-29 21:11:41 +0530

    added drawer logic

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -8,26 +8,48 @@ import {
 import PropTypes from 'prop-types';
 import NavBar from './NavBar/NavBar';
 import Login from './Login/Login';
-import Home from './Home/Home';
+import NavDrawer from './Drawer/Drawer';
+import Container from './Container/Container';
 
 const Routes = () => {
+  // Using state so that navbar can communicate with drawer
+  // mobileOpen will be used for screen width less than 600px
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  // tabletOpen will be used for screen width between 960px and 600px
+  const [tabletOpen, setTabletOpen] = React.useState(false);
+
   return (
     <Router>
+      <NavBar
+        mobileOpen={mobileOpen}
+        setMobileOpen={setMobileOpen}
+        tabletOpen={tabletOpen}
+        setTabletOpen={setTabletOpen}
+      />
       <Switch>
         <PrivateRoute exact path='/login'>
-          <NavBar />
           <Login />
         </PrivateRoute>
         <PrivateRoute path='/'>
-          <Home />
+          <NavDrawer
+            mobileOpen={mobileOpen}
+            setMobileOpen={setMobileOpen}
+            tabletOpen={tabletOpen}
+          />
+          <Container tabletOpen={tabletOpen} />
         </PrivateRoute>
       </Switch>
     </Router>
   );
 };
 
+// Private Route Component
 const PrivateRoute = ({ children, path, ...rest }) => {
   const tokenExists = !!localStorage.getItem('token');
+
+  // User cannot directly open homepage if token doesnt exist in local Storage.
+  // It aslo redirects the user to login page
+
   if (path === '/') {
     return (
       <Route
@@ -49,6 +71,10 @@ const PrivateRoute = ({ children, path, ...rest }) => {
       />
     );
   }
+
+  // User cannot directly open login page if token exists in local Storage.
+  // It aslo redirects the user to homepage
+
   if (path === '/login') {
     return (
       <Route
