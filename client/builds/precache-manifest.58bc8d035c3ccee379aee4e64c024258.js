b09c0d0d43c1c2f87b9a2d37f0d915269d677ac3
PunitLodha <punitlodha@pm.me>
2020-04-12 00:29:00 +0530

    added build files

---
+++ client/builds/precache-manifest.58bc8d035c3ccee379aee4e64c024258.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fac733d7a3cc2f04773f8c19bc53dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2efe5940f8b220b344dd",
+    "url": "/static/css/main.06cc6fd4.chunk.css"
+  },
+  {
+    "revision": "7b4e6082b9b49ad5b352",
+    "url": "/static/js/2.c4235a39.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c4235a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2efe5940f8b220b344dd",
+    "url": "/static/js/main.115c5627.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "b498e69bddf8883921edb6b0a2481fe2",
+    "url": "/static/media/Login.b498e69b.svg"
+  }
+]);
