83ef543ca9107f5f9e33df062d101c6559e9405a
Jenish Hirpara <jenishhirpara23@gmail.com>
2020-06-24 19:00:08 -0500

    Pagination

--- client/src/components/Inventory/InventoryTable.js
+++ client/src/components/Inventory/InventoryTable.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { FormControl,TextField,
-  Input } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
 import {
   Paper,
   TableBody,
@@ -13,6 +10,7 @@ import {
   TableRow,
   IconButton,
   Hidden,
+  TablePagination,
 } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
@@ -24,21 +22,7 @@ import Spinner from '../Spinner';
 import DialogBox from '../DialogBox/DialogBox';
 import { getEndPoint } from '../UtilityFunctions/Request';
 
-const useStyles = makeStyles(theme => ({
-  icons: {
-    fontSize: '2.5rem',
-    color: theme.palette.primary.light,
-    marginTop:'0.6rem'
-  },
-  search:{
-    boxShadow: '2px 2px 10px rgba(0,0,0,0.2)',
-    textAlign: 'center',
-    borderRadius: '10px',
-   padding:'0.3rem',
-   width:'400px',
-   marginLeft:'3.5rem',
-   marginBottom:'2rem',
-  },  
+const useStyles = makeStyles((theme) => ({
   paper: {
     boxShadow: '4px 4px 20px rgba(0,0,0,0.1)',
     textAlign: 'center',
@@ -93,37 +77,80 @@ export default function InventoryTable() {
   const [open, setOpen] = useState(false);
   // row to be selected on clicking the delete icon
   const [selectedRow, setSelectedRow] = useState({});
-  // list of search results got from API
-  const [searchList, setSearchList] = useState([]);
-  // search results input field
-  const [search, setSearch] = useState("");
+  // pagination
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [count, setCount] = useState(0);
+
+  const handleChangePage = async (event, newPage) => {
+    try {
+      setIsLoading(true);
+      setPage(newPage);
+      const response = await getEndPoint(
+        `/api/productlist/?limit=${rowsPerPage}&offset=${
+          newPage * rowsPerPage
+        }`,
+        null,
+        history
+      );
+      // Use utility function
+
+      // console.log("error",response) check error code here for reference
+
+      const { data } = response;
+      setCount(data.count);
+      const list = data.results.map((val) => ({
+        name: val.name,
+        quantity: val.quantity,
+        sellingPrice: val.latest_selling_price,
+        loose: val.loose,
+        id: val.id,
+        upperLimit: val.upper_limit === null ? '' : val.upper_limit,
+        lowerLimit: val.lower_limit === null ? '' : val.lower_limit,
+      }));
+      setInventoryList(list);
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChangeRowsPerPage = async (event) => {
+    try {
+      setIsLoading(true);
+      setPage(0);
+      setRowsPerPage(+event.target.value);
+      const response = await getEndPoint(
+        `/api/productlist/?limit=${+event.target.value}&offset=0`,
+        null,
+        history
+      );
+      // Use utility function
+
+      // console.log("error",response) check error code here for reference
+
+      const { data } = response;
+      setCount(data.count);
+      const list = data.results.map((val) => ({
+        name: val.name,
+        quantity: val.quantity,
+        sellingPrice: val.latest_selling_price,
+        loose: val.loose,
+        id: val.id,
+        upperLimit: val.upper_limit === null ? '' : val.upper_limit,
+        lowerLimit: val.lower_limit === null ? '' : val.lower_limit,
+      }));
+      setInventoryList(list);
+
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
   };
-
-  const handleSearch = async (data) => {
-    
-    setSearch(data) // set data
-
-    // console.log("data",data) //reference
-    
-    const response = await getEndPoint('/api/productlist/?search=' + search,null,history)
-    
-    // console.log("res2",response.data.results) // reference
-    
-    const list = response.data.results.map(val => ({
-      name: val.name,
-      quantity: val.quantity,
-      sellingPrice: val.latest_selling_price,
-      loose: val.loose, 
-      id: val.id,
-      upperLimit: val.upper_limit === null ? '' : val.upper_limit,
-      lowerLimit: val.lower_limit === null ? '' : val.lower_limit,
-    }));
-    
-    setSearchList(list); // set state
-  }
 
   const handleClose = () => {
     setOpen(false);
@@ -135,16 +162,18 @@ export default function InventoryTable() {
   const apiFetch = async () => {
     try {
       setIsLoading(true);
-     
-      const response = await getEndPoint('/api/productlist/', null, history);
+      const response = await getEndPoint(
+        '/api/productlist/?limit=10&offset=0',
+        null,
+        history
+      );
       // Use utility function
 
       // console.log("error",response) check error code here for reference
-      console.log(response.data.results)
 
-      
-      
-      const list = response.data.results.map(val => ({
+      const { data } = response;
+      setCount(data.count);
+      const list = data.results.map((val) => ({
         name: val.name,
         quantity: val.quantity,
         sellingPrice: val.latest_selling_price,
@@ -153,7 +182,6 @@ export default function InventoryTable() {
         upperLimit: val.upper_limit === null ? '' : val.upper_limit,
         lowerLimit: val.lower_limit === null ? '' : val.lower_limit,
       }));
-     
       setInventoryList(list);
       setIsLoading(false);
     } catch (e) {
@@ -170,15 +198,15 @@ export default function InventoryTable() {
   const classes = useStyles();
 
   // handle product delete
-  const handleDelete = async row => {
+  const handleDelete = async (row) => {
     setIsLoading(true);
     const { id } = row;
-    setDeletedRow(prevState => [...prevState, inventoryList.indexOf(row)]);
+    setDeletedRow((prevState) => [...prevState, inventoryList.indexOf(row)]);
     try {
       await axios.delete(`/api/productlist/${id}/`);
 
       // add success snackbar on successful request
-      const { name } = inventoryList.find(val => val.id === id);
+      const { name } = inventoryList.find((val) => val.id === id);
       setIsLoading(false);
       setSnack({
         open: true,
@@ -193,7 +221,7 @@ export default function InventoryTable() {
   };
 
   // handle product edit
-  const handleEdit = row => {
+  const handleEdit = (row) => {
     history.push('/updateproduct', {
       name: row.name,
       sellingPrice: row.sellingPrice,
@@ -206,17 +234,8 @@ export default function InventoryTable() {
 
   return (
     <>
-   
       {isLoading ? <Spinner /> : null}
-      
-      <form  className={classes.search} noValidate autoComplete="off">
-        <SearchIcon className = {classes.icons}/> 
-        <TextField onChange={e => handleSearch(e.target.value)} style={{width:"350px"}} id="standard-basic" label="Search" variant="filled" />
-       
-      </form>
-       
-    
-      {search == "" ? <> <Paper className={classes.paper}>
+      <Paper className={classes.paper}>
         <TableContainer>
           <Table className={classes.table} aria-label='simple table'>
             <TableHead>
@@ -273,6 +292,15 @@ export default function InventoryTable() {
             </TableBody>
           </Table>
         </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10]}
+          component='div'
+          count={count}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
       </Paper>
       <DialogBox
         open={open}
@@ -280,75 +308,7 @@ export default function InventoryTable() {
         selectedRow={selectedRow}
         handleDelete={handleDelete}
         number='1'
-      /></>:<>
-      
-      <Paper className={classes.paper}>
-        <TableContainer>
-          <Table className={classes.table} aria-label='simple table'>
-            <TableHead>
-              <TableRow>
-                <TableCell />
-                <TableCell>Product</TableCell>
-                <TableCell align='right'>Items</TableCell>
-                <TableCell align='right'>Price (Rs)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {searchList.map((row, index) => (
-                <TableRow
-                  key={row.name}
-                  hover
-                  className={deletedRow.includes(index) ? 'delete' : ''}
-                >
-                  <TableCell className={classes.firstColumn}>
-                    <Hidden xsDown>
-                      <IconButton
-                        onClick={() => {
-                          handleEdit(row);
-                        }}
-                      >
-                        <EditIcon />
-                      </IconButton>
-                      <IconButton
-                        onClick={() => {
-                          setSelectedRow(row);
-                          handleClickOpen();
-                        }}
-                      >
-                        <DeleteIcon />
-                      </IconButton>
-                    </Hidden>
-                    <Hidden smUp>
-                      <MobileEditMenu
-                        handleDelete={() => {
-                          setSelectedRow(row);
-                          handleClickOpen();
-                        }}
-                        handleEdit={handleEdit}
-                        row={row}
-                      />
-                    </Hidden>
-                  </TableCell>
-                  <TableCell>{row.name}</TableCell>
-                  <TableCell align='right'>{row.quantity}</TableCell>
-                  <TableCell align='right'>
-                    {row.sellingPrice || 'Not Set'}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-      <DialogBox
-        open={open}
-        handleClose={handleClose}
-        selectedRow={selectedRow}
-        handleDelete={handleDelete}
-        number='1'
-      /></>}
-     
-     
+      />
     </>
   );
 }
