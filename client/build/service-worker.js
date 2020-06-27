28591333cfe5e40430a7e36b1953166ed3d39912
PunitLodha <punitlodha@pm.me>
2020-06-27 15:37:14 +0530

    updated build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f9673b6bc0158f272ba63fe67c4e8c0.js"
+  "/precache-manifest.4e6ef8fb5650c0908014deccf5f8ca05.js"
 );
 
 self.addEventListener('message', (event) => {
